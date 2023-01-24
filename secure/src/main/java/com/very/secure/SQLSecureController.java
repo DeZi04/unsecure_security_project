f71dbfa584d2182178ae5a7cf989aa3ecc98da82
Lenny <lenny@familie-angst.ch>
2023-01-24 11:25:17 +0100

    🔒️ Implement secure variant

--- secure/src/main/java/com/very/secure/SQLSecureController.java
+++ secure/src/main/java/com/very/secure/SQLSecureController.java
@@ -1,20 +1,39 @@
 package com.very.secure;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.very.secure.model.Entry;
 
 @Controller()
 public class SQLSecureController {
-  @GetMapping("/sql/secure")
-  public String sqlPage() {
-    return "/sql";
-  }
 
-  @PostMapping("/sql/secure/input")
-  public String input(@RequestParam String input, Model model) {
-    return "/sql";
-  }
+	@Autowired
+	EntityManager entityManager;
+
+	@GetMapping("/sql/secure")
+	public String sqlPage() {
+		return "/sql";
+	}
+
+	@Transactional
+	@PostMapping("/sql/secure/input")
+	public String input(@RequestParam String input, Model model) {
+		var entry = new Entry();
+		entry.setVal("Test123");
+		entityManager.persist(entry);
+
+		var results = entityManager.createNativeQuery("select val from entry where val = ?")
+				.setParameter(1, input).getResultList();
+		if (!results.isEmpty()) {
+			System.out.println("sus");
+			model.addAttribute("text", results.get(0));
+		}
+		return "/sql";
+	}
 }
